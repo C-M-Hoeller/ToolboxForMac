b0b31b12f9205da6f42937efe650c83188b52cd9
Christoph Hoeller <c.m.hoeller@gmail.com>
2020-06-14 20:16:17 +0200

    Improved Structur

--- ToolsForMac.sh
+++ ToolsForMac.sh
@@ -11,6 +11,9 @@ declare -a BrowserFormulae=("google-chrome" "firefox" "cleanmymac")
 declare -a UtilitiesFormulae=("numi" "oversight" "rocket" "hiddenbar" "monitorcontrol")
 declare -a ExtraFormulae=("fork" "setapp" "visual-studio" "calibre" "unetbootin" "drawio" "responsively" "azure-data-studio" "filezilla" "logitech-options" "itsycal" "endpoint_security_vpn" "xtrafinder" "divvy")
 
+#---------------------------Helper Functions------------------------------------
+function join { local IFS="$1"; shift; echo "$*"; }
+
 #--------------------------------Functions--------------------------------------
 # brew cask install
 bci(){
@@ -31,155 +34,46 @@ check_individually(){
 # Check Yes No Individually
 check(){
     echo "Do you want to install $1"
-    echo "This package includes ($2)"
+    shift
+    list=$(join , $@)
+    echo "This package includes: \n $list" 
     select yn in "Yes" "No" "Individually"; do
         case $yn in
-            Yes ) $3; break;;
-            Individually ) $4; break;;
+            Yes ) install_package $@; break;;
+            Individually ) install_package_individually $@; break;;
             No ) break;;
         esac
     done
 }
-#-------------------------------CASK Formulae-----------------------------------
-# Office / Documentation
-install_office(){
-    for formula in "${OfficeFormulae[@]}"
+# Install packages
+install_package(){
+    for formula in "$@"
     do
         bci $formula
-    done
-}
-install_office_individually(){
-    for formula in "${OfficeFormulae[@]}"
-    do
-        check $formula
     done
 }
 
-# Cloud
-install_cloud(){
-    for formula in "${CloudFormulae[@]}"
+install_package_individually(){
+    for formula in "$@"
     do
-        bci $formula
-    done
-}
-install_cloud_individually(){
-    for formula in "${CloudFormulae[@]}"
-    do
-        check $formula
+        check_individually $formula
     done
 }
 
-# Messenger
-install_messenger(){
-    for formula in "${MessengerFormulae[@]}"
-    do
-        bci $formula
-    done
-}
-install_messenger_individually(){
-    for formula in "${MessengerFormulae[@]}"
-    do
-        check $formula
-    done
-}
-
-# Media
-install_media(){
-    for formula in "${MediaFormulae[@]}"
-    do
-        bci $formula
-    done
-}
-install_media_individually(){
-    for formula in "${MediaFormulae[@]}"
-    do
-        check $formula
-    done
-}
-
-# Design
-install_design(){
-    for formula in "${DesignFormulae[@]}"
-    do
-        bci $formula
-    done
-}
-install_desgin_individually(){
-    for formula in "${DesignFormulae[@]}"
-    do
-        check $formula
-    done
-}
-
-# Development
-install_development(){
-    for formula in "${DeveloperFormulae[@]}"
-    do
-        bci $formula
-    done
-}
-install_development_individually(){
-    for formula in "${DeveloperFormulae[@]}"
-    do
-        check $formula
-    done
-}
-
-# Browser
-install_browser(){
-    for formula in "${BrowserFormulae[@]}"
-    do
-        bci $formula
-    done
-}
-install_browser_individually(){
-    for formula in "${BrowserFormulae[@]}"
-    do
-        check $formula
-    done
-}
-
-# Utilities
-install_utilities(){
-    for formula in "${UtilitiesFormulae[@]}"
-    do
-        bci $formula
-    done
-}
-install_utilities_individually(){
-    for formula in "${UtilitiesFormulae[@]}"
-    do
-        check $formula
-    done
-}
-
-# ExtraFormulae
-install_extras(){
-    for formula in "${ExtraFormulae[@]}"
-    do
-        bci $formula
-    done
-}
-install_extras_individually(){
-    for formula in "${ExtraFormulae[@]}"
-    do
-        check $formula
-    done
-}
 #-------------------------------------------------------------------------------
 # Install Software Packages
 install_software(){
-    check "Office / Documentation"         $OfficeFormulae         "install_office"        "install_office_individually"
-    check "Cloud"                          $CloudFormulae          "install_cloud"         "install_cloud_individually"
-    check "Messenger / Social Networks"    $MessengerFormulae      "install_messenger"     "install_messenger_individually"
-    check "Music / Video / Media"          $MediaFormulae          "install_media"         "install_media_individually"
-    check "Design"                         $DesignFormulae         "install_design"        "install_desgin_individually"
-    check "Development / IT-Tools"         $DeveloperFormulae      "install_development"   "install_development_individually"
-    check "Browser"                        $BrowserFormulae        "install_browser"       "install_browser_individually"
-    check "Utilities"                      $UtilitiesFormulae      "install_utilities"     "install_utilities_individually"
-    check "Extras"                         $ExtraFormulae          "install_extras"        "install_extras_individually"
+    check "Office / Documentation"         "${OfficeFormulae[@]}"
+    check "Cloud"                          "${CloudFormulae[@]}"
+    check "Messenger / Social Networks"    "${MessengerFormulae[@]}"
+    check "Music / Video / Media"          "${MediaFormulae[@]}"
+    check "Design"                         "${DesignFormulae[@]}"
+    check "Development / IT-Tools"         "${DeveloperFormulae[@]}"
+    check "Browser"                        "${BrowserFormulae[@]}"
+    check "Utilities"                      "${UtilitiesFormulae[@]}"
+    check "Extras"                         "${ExtraFormulae[@]}"
 }
-
+#-------------------------------------------------------------------------------
 
 # Xcode Monokai Theme
 install_monokai_xcode_theme(){
@@ -233,16 +127,6 @@ if [[ $? != 0 ]] ; then
         case $yn in
             Yes ) sh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"; break;;
             No ) brewCheck = false; break;;
-        esac
-    done
-fi
-
-if ! brew info cask &>/dev/null; then
-    echo "Do you want to install Cask?"
-    select yn in "Yes" "No"; do
-        case $yn in
-            Yes ) brew install cask; break;;
-            No ) break;;
         esac
     done
 fi
